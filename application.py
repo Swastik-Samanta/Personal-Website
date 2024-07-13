a6ca517a840994303fb582eda741147480efe16e
Swastik1012 <Swastik1012@gmail.com>
2024-07-13 17:14:46 -0400

    minor changes

--- application.py
+++ application.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, request, jsonify # type: ignore
-from flask_cors import CORS # type: ignore
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 from chat import get_response
 
-app = Flask(__name__)
-CORS(app)
+application = Flask(__name__)
+CORS(application)
 
 
-@app.get("/")
+@application.get("/")
 def index_get():
     return render_template("index.html")
 
-@app.post("/predict")
+@application.post("/predict")
 def predict():
     text = request.get_json().get("message")
     # Check if text is valid.
@@ -19,4 +19,4 @@ def predict():
     return jsonify(message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
